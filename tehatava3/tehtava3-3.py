d7e2543389424b784cecc412e3cba5700a65479a
Miska Lohikoski <miska.lohikoski@proton.me>
2026-08-18 13:49:17 +0300

    lisätää tehtävät 3

---
+++ tehatava3/tehtava3-3.py
@@ -0,0 +1,8 @@
+#kysyy suorakulmion kannan
+kanta = int(input("Mikä suorakulmion kanta? "))
+#kysyy suorakulmion korkeuden
+korkeus = int(input("Mikä suorakulmion korkeus? "))
+#printtaa korkeus
+print("piiri:", kanta*2+korkeus*2)
+#printtaa pintaala
+print("pinta-ala:", kanta*korkeus)
