09f79bf327d4322ac09a06e5898b1e397108777c
Miska Lohikoski <miska.lohikoski@proton.me>
2026-08-18 14:10:54 +0300

    tehtiin tehtävä 4

---
+++ tehatava3/tehtava3-4.py
@@ -0,0 +1,11 @@
+#kysyy numero 1
+num1 = int(input("Mikä numero yksi? "))
+#kysyy numero 2
+num2 = int(input("Mikä numero kaksi? "))
+#kysyy numero 3
+num3 = int(input("Mikä numero kolme? "))
+
+#printtaa summa, tulo ja keskiarvo
+print("summa", num1+num2+num3)
+print("tulo", num1*num2*num3)
+print("keskiarvo", (num1+num2+num3)/3)
